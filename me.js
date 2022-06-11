2b11a7fd55c46675d821f650ced29813f9abe6be
Nelson.H <hilary.nelsonn@yahoo.com>
2022-06-11 18:32:42 +0100

    my first commit

---
+++ me.js
@@ -0,0 +1,19 @@
+//Given an array write a function to print every third element of the array .
+/*let total = 1;
+function printEveryThirdElement(arr){
+    for(let i = 0; i < arr.length; i ++){
+        for (let j = 0; j < arr[i].length; j ++){
+            total = arr[i][j];
+        }
+    }
+    console.log(total); 
+    }
+
+printEveryThirdElement([2,4,5,6,6,7,7,10,101,0,1]);// 5 7 101
+printEveryThirdElement([2,4,5,6,6,7,7,10,101,0,107,55]);// 5 7 101 55*/
+
+let a = true;
+let g = false;
+for(let i = a; i <= g; i ++){
+    console.log(i);
+};
